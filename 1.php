a75d004b38173236560f2b82b315a946743f6b3f
kovalin <9820498@gmail.com>
2021-06-07 17:58:54 +0500

    Add 1 Task

---
+++ 1.php
@@ -0,0 +1,28 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer[]
+     */
+    function twoSum($nums, $target) {
+        foreach($nums as $i=>$n) {
+            $_nums = $nums;
+            unset($_nums[$i]);
+            $s = array_search($target-$n, $_nums);
+            if ($s !== false) {
+                return [$i, $s];
+            }
+        }
+        return false;
+    }
+}
+
+$obj = new Solution();
+
+print_r($obj->twoSum([3,2,4], 6));
+
+///*$a = 0;
+//var_dump($a !== false);*/
